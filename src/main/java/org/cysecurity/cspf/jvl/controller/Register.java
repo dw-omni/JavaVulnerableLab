1fcb1a38e180648e56ea4e37ec2a6c7b9f8bdbf5
dw-omni <damian.watrach@omnilogy.pl>
2023-09-26 13:32:20 +0200

    Update Register.java

--- src/main/java/org/cysecurity/cspf/jvl/controller/Register.java
+++ src/main/java/org/cysecurity/cspf/jvl/controller/Register.java
@@ -56,9 +56,19 @@ public class Register extends HttpServlet {
                                   
                                    Statement stmt = con.createStatement();  
                                   stmt.executeUpdate("INSERT into users(username, password, email, About,avatar,privilege,secretquestion,secret) values ('"+user+"','"+pass+"','"+email+"','"+about+"','default.jpg','user',1,'"+secret+"')");
-                                       stmt.executeUpdate("INSERT into UserMessages(recipient, sender, subject, msg) values ('\"+user+\"','admin','Hi','Hi<br/> This is admin of this page. <br/> Welcome to Our Forum')");
+//                                       stmt.setString(1, user);
+//                                       stmt.setString(1, pass);
+//                                       stmt.setString(1, email);
+//                                       stmt.setString(1, about);
+//                                       stmt.setString(1, secret);
+                                       stmt.executeUpdate("INSERT into UserMessages(recipient, sender, subject, msg) values ('"+user+"','admin','Hi','Hi<br/> This is admin of this page. <br/> Welcome to Our Forum')");
 //                                       ? instead of "+user"
 //                                       stmt.setString(1, user);
+//                                       stmt.setString(1, pass);
+//                                       stmt.setString(1, email);
+//                                       stmt.setString(1, about);
+//                                       stmt.setString(1, secret);
+
 
                                     response.sendRedirect("index.jsp");
 
